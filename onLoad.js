0429a457a39b51e4a83c6fc6785c8c64b0dc1b96
Kiel <kielmitchell8@gmail.com>
2024-12-20 12:14:58 +0000

    button fix

--- onLoad.js
+++ onLoad.js
@@ -1,19 +1,19 @@
 function adjustStyles() {
-    const allElements = document.querySelectorAll("*");
+
     try {
+        const allButtons = document.querySelectorAll("button");
+        allButtons.forEach((button) => {
+            button.classList = ""
+        })
+        const allElements = document.querySelectorAll("*");
         allElements.forEach((elem) => {
-            elem.classList = ""
             elem.style.backgroundColor = "black"
             elem.style.color = "white"
             elem.style.borderRadius = "0"
             elem.style.fontFamily = "Georgia, serif"
             elem.style.accentColor = "white"
-            elem.style.backgroundImage = "none"
             elem.style.caretColor = "white"
             elem.style.columnRuleColor = "white"
-
-            // elem.style.backgroundColor = "black"
-            // elem.style.backgroundColor = "black"
         })
     } catch (err) {
         console.log(err)
