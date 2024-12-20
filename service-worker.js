37349b709be5bb863559877f40241c57b701d69a
Kiel <kielmitchell8@gmail.com>
2024-12-20 11:34:31 +0000

    follows serviceworker example

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+function adjustStyles() {
+    const allElements = document.querySelectorAll("*");
+    try {
+        allElements.forEach((elem) => {
+            elem.style.backgroundColor = "black"
+            elem.style.color = "white"
+            elem.style.borderRadius = "0"
+            // elem.style.fontFamily = "Georgia, serif"
+            // if img
+            // if(elem.attributes.)
+            // if div
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    if (!tab.url.includes('chrome://')) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: adjustStyles
+        });
+    }
+});
