ad417309df2e274783355d37a5675d8fb6be2aa6
Kiel <kielmitchell8@gmail.com>
2024-12-20 12:05:44 +0000

    tries classlist remove

--- onLoad.js
+++ onLoad.js
@@ -2,14 +2,16 @@ function adjustStyles() {
     const allElements = document.querySelectorAll("*");
     try {
         allElements.forEach((elem) => {
+            elem.classList = ""
             elem.style.backgroundColor = "black"
             elem.style.color = "white"
             elem.style.borderRadius = "0"
-            // elem.style.fontFamily = "Georgia, serif"
-            // if img
-            // if(elem.attributes.)
-            // if div
-            // elem.style.backgroundColor = "black"
+            elem.style.fontFamily = "Georgia, serif"
+            elem.style.accentColor = "white"
+            elem.style.backgroundImage = "none"
+            elem.style.caretColor = "white"
+            elem.style.columnRuleColor = "white"
+
             // elem.style.backgroundColor = "black"
             // elem.style.backgroundColor = "black"
         })
