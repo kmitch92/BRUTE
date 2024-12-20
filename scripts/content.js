fe854157a8d60edf29f0d6fae1cdc6edba9eede0
Kiel <kielmitchell8@gmail.com>
2024-12-20 10:43:21 +0000

    attempts to add button

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,24 @@
-const allElements = document.querySelectorAll("*");
-allElements.forEach((elem) => {
-    elem.style.backgroundColor = "black"
-    elem.style.color = "white"
-    elem.style.borderRadius = "0"
-    elem.style.fontFamily = "Georgia, serif"
-    // elem.style.backgroundColor = "black"
-    // elem.style.backgroundColor = "black"
-    // elem.style.backgroundColor = "black"
+const button = document.getElementById("brute-button")
+button.addEventListener("click", () => {
+    button.value = button.value ? false : true
 })
+
+if (button.value) {
+    try {
+        const allElements = document.querySelectorAll("*");
+        allElements.forEach((elem) => {
+            elem.style.backgroundColor = "black"
+            elem.style.color = "white"
+            elem.style.borderRadius = "0"
+            elem.style.fontFamily = "Georgia, serif"
+            // if img
+            // if(elem.attributes.)
+            // if div
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
