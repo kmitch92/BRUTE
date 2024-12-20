7ddf52776b3cbad2251d2ef8e0aea7491795487b
Kiel <kielmitchell8@gmail.com>
2024-12-20 10:28:40 +0000

    adds more css rules

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,10 @@
-document.body.style.backgroundColor = 'red';
+const allElements = document.querySelectorAll("*");
+allElements.forEach((elem) => {
+    elem.style.backgroundColor = "black"
+    elem.style.color = "white"
+    elem.style.borderRadius = "0"
+    elem.style.fontFamily = "Georgia, serif"
+    // elem.style.backgroundColor = "black"
+    // elem.style.backgroundColor = "black"
+    // elem.style.backgroundColor = "black"
+})
