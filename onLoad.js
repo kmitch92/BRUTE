3aad6c16d95fac673f4d1f9ef614cb23d5940e65
Kiel <kielmitchell8@gmail.com>
2024-12-20 11:55:38 +0000

    fixing errs

--- onLoad.js
+++ onLoad.js
@@ -1,2 +1,20 @@
-import { adjustStyles } from "./service-worker";
+function adjustStyles() {
+    const allElements = document.querySelectorAll("*");
+    try {
+        allElements.forEach((elem) => {
+            elem.style.backgroundColor = "black"
+            elem.style.color = "white"
+            elem.style.borderRadius = "0"
+            // elem.style.fontFamily = "Georgia, serif"
+            // if img
+            // if(elem.attributes.)
+            // if div
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+            // elem.style.backgroundColor = "black"
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
 adjustStyles() 
